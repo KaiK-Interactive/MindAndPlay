afc759b4848c09607926924d1e2d1052e92f0828
KKaiKK <KKaiKK@web.de>
2015-03-16 19:50:20 +0100

    Implementierung der Phonegap Grundstruktur
    Implementierung der Phonegap Grundstruktur sowie des Logos und eines
    ersten Designs.

---
+++ MindAndPlay/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes.dex : \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes\com\phonegap\helloworld\BuildConfig.class \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes\com\phonegap\helloworld\HelloWorld.class \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes\com\phonegap\helloworld\R$attr.class \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes\com\phonegap\helloworld\R$drawable.class \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes\com\phonegap\helloworld\R$string.class \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes\com\phonegap\helloworld\R$xml.class \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes\com\phonegap\helloworld\R.class \
+D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\dexedLibs\classes-eb8a38bb2e0849759ed80f3050b36874.jar \
