cf30f9fa118b04dd53583e7a77bf81adb0a5bd78
KaiK <KKaiKK@web.de>
2015-08-26 18:15:52 +0200

    Some files removed

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\HelloWorld-debug-unaligned.apk : \
-D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\HelloWorld.ap_ \
-D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\ant-build\classes.dex \
-D:\Projekte\GitHub\MindAndPlay\MindAndPlay\MindAndPlay\platforms\android\CordovaLib\ant-build\classes.jar \
